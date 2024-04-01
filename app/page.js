d6f8daf05c55208679d347bcec864412ea8580bc
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-04-01 13:46:52 -0300

    Create auth with clerk

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Button>Me clica vai !!!</Button>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
