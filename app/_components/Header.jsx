02fde73d034ecd1a7449eb8cb5f1d915e6a14d84
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-04-01 15:32:21 -0300

    Add header component

---
+++ app/_components/Header.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
+
+const Header = () => {
+  const { user, isSignedIn } = useUser();
+  return (
+    <header className="h-24 flex justify-between items-center p-6 md:px-20 shadow-sm  ">
+      <Image src="/foodie-logo.png" width={150} height={150} />
+
+      <div className="hidden md:flex border p-2 rounded-lg bg-gray-200 w-96">
+        <input
+          type="text"
+          className="bg-transparent w-full outline-none"
+          placeholder="Search Food ..."
+        />
+        <Search />
+      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-5 ">
+          <SignInButton mode="modal">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+          <SignUpButton mode="modal">
+            <Button>Sign Up</Button>
+          </SignUpButton>
+        </div>
+      )}
+    </header>
+  );
+};
+export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "./Provider";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={outfit.className}>{children}</body>
+        <body className={outfit.className}>
+          <Provider>{children}</Provider>
+        </body>
       </html>
     </ClerkProvider>
   );
